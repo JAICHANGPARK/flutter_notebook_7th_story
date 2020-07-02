6877c20b6408a73e72a0d77fc1ec0017d6b1dd55
Dreamwalker <aristojeff@gmail.com>
2020-07-02 19:47:33 +0900

    Update travel_main_page.dart

--- lib/ep500_travel_app/travel_main_page.dart
+++ lib/ep500_travel_app/travel_main_page.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class TravelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+       home: TravelHomePage(),
+    );
   }
 }
+
