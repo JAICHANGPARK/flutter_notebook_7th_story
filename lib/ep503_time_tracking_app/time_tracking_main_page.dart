5948319b154c5fea58425841def376142df4698e
Dreamwalker <aristojeff@gmail.com>
2020-07-05 19:04:04 +0900

    Update time_tracking_main_page.dart

--- lib/ep503_time_tracking_app/time_tracking_main_page.dart
+++ lib/ep503_time_tracking_app/time_tracking_main_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class TimeTrackingMainPage extends StatefulWidget {
+  @override
+  _TimeTrackingMainPageState createState() => _TimeTrackingMainPageState();
+}
+
+class _TimeTrackingMainPageState extends State<TimeTrackingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
