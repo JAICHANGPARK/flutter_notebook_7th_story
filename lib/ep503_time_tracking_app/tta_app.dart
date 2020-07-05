c7118c7c612fe01f56c0aaf66fd1189442bdedd4
Dreamwalker <aristojeff@gmail.com>
2020-07-05 19:03:24 +0900

    Update tta_app.dart

--- lib/ep503_time_tracking_app/tta_app.dart
+++ lib/ep503_time_tracking_app/tta_app.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class TimeTrackingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      
+    );
   }
 }
