db42470178a480b14366c1e7249a3460a3ce14f1
Dreamwalker <aristojeff@gmail.com>
2020-07-08 20:28:54 +0900

    Update time_tracking_main_page.dart

--- lib/ep503_time_tracking_app/time_tracking_main_page.dart
+++ lib/ep503_time_tracking_app/time_tracking_main_page.dart
@@ -331,7 +331,7 @@ class _TimeTrackingMainPageState extends State<TimeTrackingMainPage> {
                                       width: 100,
                                       height: 34,
                                       decoration: BoxDecoration(
-                                          color: Colors.orange
+                                          
                                       ),
                                       child: Center(
                                         child: Text("YEARLY"),
