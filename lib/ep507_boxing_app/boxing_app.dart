a1f1b0379fa72c82d331650b0473c639743e36ab
Dreamwalker <aristojeff@gmail.com>
2020-07-09 20:29:15 +0900

    Update boxing_app.dart

--- lib/ep507_boxing_app/boxing_app.dart
+++ lib/ep507_boxing_app/boxing_app.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class BoxingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: BoxingHomePage(),
+    );
   }
 }
