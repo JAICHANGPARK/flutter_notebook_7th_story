f66e3154b7f160bb3650aa9a4b3eae3403a3af7d
Dreamwalker <aristojeff@gmail.com>
2020-07-09 20:30:48 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_7th_story/ep503_time_tracking_app/tta_app.dart';
+import 'ep507_boxing_app/boxing_app.dart';
 
 void main() {
-  runApp(TimeTrackingApp());
+  runApp(BoxingApp());
 }
