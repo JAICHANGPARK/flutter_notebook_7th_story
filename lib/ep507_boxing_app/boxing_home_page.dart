adf281aff57eff28abc0070d857c2fdf0cd53ca8
Dreamwalker <aristojeff@gmail.com>
2020-07-09 20:29:41 +0900

    Update boxing_home_page.dart

--- lib/ep507_boxing_app/boxing_home_page.dart
+++ lib/ep507_boxing_app/boxing_home_page.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+
+class BoxingHomePage extends StatefulWidget {
+  @override
+  _BoxingHomePageState createState() => _BoxingHomePageState();
+}
+
+class _BoxingHomePageState extends State<BoxingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
