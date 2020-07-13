810ab725ef31c13fdd6d647b97d8aca811f3be6b
Dreamwalker <aristojeff@gmail.com>
2020-07-13 20:09:00 +0900

    Update main_custom_clipper.dart

--- lib/ep511_project_management_app/main_custom_clipper.dart
+++ lib/ep511_project_management_app/main_custom_clipper.dart
@@ -1,19 +1,22 @@
-
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class MainCustomClipper extends CustomClipper<Path>{
+class MainCustomClipper extends CustomClipper<Path> {
   double radius = 50.0;
+
   @override
   Path getClip(Size size) {
     // TODO: implement getClip
     Path path = Path();
     path.lineTo(size.width - radius, 0);
     path.arcTo(Rect.fromPoints(Offset(size.width - radius, 0),
-    Offset(size.width , radius)),
-        1.5 * pi, 0.5 * pi, true);
+        Offset(size.width, radius)), 1.5 * pi, 0.5 * pi, true);
     path.lineTo(size.width, size.height - radius);
+    path.arcTo(Rect.fromCircle(center: Offset(
+      size.width - radius,
+      size.height - radius,
+    ), radius: radius), startAngle, sweepAngle, forceMoveTo)
     path.close();
 
     return path;
@@ -24,6 +27,4 @@ class MainCustomClipper extends CustomClipper<Path>{
     // TODO: implement shouldReclip
     return false;
   }
-
-
 }
