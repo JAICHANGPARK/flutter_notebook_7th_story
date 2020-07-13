2c39ae746bfd86976802895b7580dd971cc457aa
Dreamwalker <aristojeff@gmail.com>
2020-07-13 19:53:40 +0900

    Update project_main_page.dart

--- lib/ep511_project_management_app/project_main_page.dart
+++ lib/ep511_project_management_app/project_main_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class PMMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+    );
   }
 }
