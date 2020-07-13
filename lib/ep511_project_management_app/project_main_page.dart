147eadf077c63e0a0b063a4f52a4cfe93e011481
Dreamwalker <aristojeff@gmail.com>
2020-07-13 19:53:17 +0900

    Update project_main_page.dart

--- lib/ep511_project_management_app/project_main_page.dart
+++ lib/ep511_project_management_app/project_main_page.dart
@@ -4,6 +4,6 @@ import 'package:flutter/material.dart';
 class PMMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
