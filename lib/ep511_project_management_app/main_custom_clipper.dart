69e2ca9f4c62e70ee5da3653661b16fb3c747b43
Dreamwalker <aristojeff@gmail.com>
2020-07-13 20:00:37 +0900

    Update main_custom_clipper.dart

--- lib/ep511_project_management_app/main_custom_clipper.dart
+++ lib/ep511_project_management_app/main_custom_clipper.dart
@@ -2,5 +2,17 @@
 import 'package:flutter/material.dart';
 
 class MainCustomClipper extends CustomClipper{
+  @override
+  getClip(Size size) {
+    // TODO: implement getClip
+  Path path = Path();
+  return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    // TODO: implement shouldReclip
+    return false;
+  }
 
 }
