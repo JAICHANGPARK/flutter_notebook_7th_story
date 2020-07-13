6527af2b24e5033911fc2102da34cfda8b03e21e
Dreamwalker <aristojeff@gmail.com>
2020-07-13 20:05:37 +0900

    Update main_custom_clipper.dart

--- lib/ep511_project_management_app/main_custom_clipper.dart
+++ lib/ep511_project_management_app/main_custom_clipper.dart
@@ -2,11 +2,12 @@
 import 'package:flutter/material.dart';
 
 class MainCustomClipper extends CustomClipper<Path>{
+  double radius = 50.0;
   @override
   Path getClip(Size size) {
     // TODO: implement getClip
     Path path = Path();
-    path.lineTo(0, size.height);
+    path.lineTo(size.width - radius, 0);
     path.lineTo(size.width, size.height);
     path.close();
 
