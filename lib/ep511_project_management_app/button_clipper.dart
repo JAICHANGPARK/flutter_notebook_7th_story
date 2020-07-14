135adf2b873c20c589eeaae4dffd3eba1d452200
Dreamwalker <aristojeff@gmail.com>
2020-07-14 20:40:01 +0900

    Update button_clipper.dart

--- lib/ep511_project_management_app/button_clipper.dart
+++ lib/ep511_project_management_app/button_clipper.dart
@@ -7,13 +7,15 @@ class ButtonClipper extends CustomClipper<Path>{
   @override
   Path getClip(Size size) {
     // TODO: implement getClip
-    throw UnimplementedError();
+    Path path = Path();
+    path.close();
+    return path;
   }
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) {
     // TODO: implement shouldReclip
-    throw UnimplementedError();
+   return false;
   }
   
 }
