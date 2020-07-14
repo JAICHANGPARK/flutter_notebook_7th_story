343cfed66a47c53045c226758d3d3e577f2329b2
Dreamwalker <aristojeff@gmail.com>
2020-07-14 20:39:17 +0900

    Create button_clipper.dart

---
+++ lib/ep511_project_management_app/button_clipper.dart
@@ -0,0 +1,19 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class ButtonClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    // TODO: implement getClip
+    throw UnimplementedError();
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    throw UnimplementedError();
+  }
+  
+}
