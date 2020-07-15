8caba0925f4dcfc9cdb687bfdefbeb83ce911752
Dreamwalker <aristojeff@gmail.com>
2020-07-15 21:14:34 +0900

    Update button_clipper.dart

--- lib/ep511_project_management_app/button_clipper.dart
+++ lib/ep511_project_management_app/button_clipper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ButtonClipper extends CustomClipper<Path> {
+  double radius = 25.0;
   @override
   Path getClip(Size size) {
     // TODO: implement getClip
