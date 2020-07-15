3c47f2158716ec096e2194a8bf37d8bf1f45e7d9
Dreamwalker <aristojeff@gmail.com>
2020-07-15 21:23:00 +0900

    Update button_clipper.dart

--- lib/ep511_project_management_app/button_clipper.dart
+++ lib/ep511_project_management_app/button_clipper.dart
@@ -9,7 +9,7 @@ class ButtonClipper extends CustomClipper<Path> {
     Path path = Path()
     ..lineTo(size.width - radius, radius)
     ..arcTo(Rect.fromPoints(Offset(size.width - radius, 0), Offset(size.width, radius)), 1.5 * pi, 0.5 * pi, true)
-    ..lineTo(size.width - (2 *radius ), size.height);
+    ..lineTo(size.width - (radius ), size.height);
     
     path.close();
     return path;
