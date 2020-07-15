f27b906f848f38d3e5afffeb3b62730827076b24
Dreamwalker <aristojeff@gmail.com>
2020-07-15 21:24:05 +0900

    Update button_clipper.dart

--- lib/ep511_project_management_app/button_clipper.dart
+++ lib/ep511_project_management_app/button_clipper.dart
@@ -3,14 +3,17 @@ import 'dart:math';
 
 class ButtonClipper extends CustomClipper<Path> {
   double radius = 25.0;
+
   @override
   Path getClip(Size size) {
     // TODO: implement getClip
     Path path = Path()
-    ..lineTo(size.width - radius, radius)
-    ..arcTo(Rect.fromPoints(Offset(size.width - radius, 0), Offset(size.width, radius)), 1.5 * pi, 0.5 * pi, true)
-    ..lineTo(size.width - (radius ), size.height);
-    
+      ..lineTo(size.width - radius, radius)
+      ..arcTo(Rect.fromPoints(Offset(size.width - radius, 0), Offset(size.width, radius)), 1.5 * pi, 0.5 * pi, true)
+      ..lineTo(size.width - (radius), size.height)
+      ..arcTo(Rect.fromPoints(Offset(size.width - radius, size.height), Offset(size.width, size.height)), 1.5 * pi,
+          0.5 * pi, true);
+
     path.close();
     return path;
   }
