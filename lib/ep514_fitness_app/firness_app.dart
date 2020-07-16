35be136ccdb80b1739a2b3258d59a3f6f75adb2d
Dreamwalker <aristojeff@gmail.com>
2020-07-16 20:35:58 +0900

    Update firness_app.dart

---
+++ lib/ep514_fitness_app/firness_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FitnessApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FitnessMainPage(),
+    );
+  }
+}
