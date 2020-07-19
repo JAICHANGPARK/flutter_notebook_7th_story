05b6514bf466b4de21c878157947408d308283db
Dreamwalker <aristojeff@gmail.com>
2020-07-19 19:47:29 +0900

    Create lichi_app_home_page.dart

---
+++ lib/ep517_lichi_app_clone/lichi_app_home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class LichiAppHomePage extends StatefulWidget {
+  @override
+  _LichiAppHomePageState createState() => _LichiAppHomePageState();
+}
+
+class _LichiAppHomePageState extends State<LichiAppHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
