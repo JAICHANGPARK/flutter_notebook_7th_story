f03a2af21096a42ed2ad7cdd01be1704e0208ecb
Dreamwalker <aristojeff@gmail.com>
2020-07-20 20:04:23 +0900

    Update detail_page.dart

--- lib/ep517_lichi_app_clone/detail_page.dart
+++ lib/ep517_lichi_app_clone/detail_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
-  
+  final String title;
+  final String imgPath;
+  final String price;
+
+  DetailPage({this.title, this.imgPath, this.price});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      
+    );
   }
 }
