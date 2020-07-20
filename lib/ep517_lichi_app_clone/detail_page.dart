a83811c84753d44a1cf23a942984f053acd40625
Dreamwalker <aristojeff@gmail.com>
2020-07-20 20:05:17 +0900

    Update detail_page.dart

--- lib/ep517_lichi_app_clone/detail_page.dart
+++ lib/ep517_lichi_app_clone/detail_page.dart
@@ -10,7 +10,19 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Container(
+              
+            ),
+          )
+        ],
+      ),
     );
   }
 }
