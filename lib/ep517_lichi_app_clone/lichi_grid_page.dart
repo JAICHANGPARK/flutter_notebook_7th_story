340e004df2eb0a85471da53af4443ec420d4c05e
Dreamwalker <aristojeff@gmail.com>
2020-07-21 20:24:17 +0900

    Update lichi_grid_page.dart

--- lib/ep517_lichi_app_clone/lichi_grid_page.dart
+++ lib/ep517_lichi_app_clone/lichi_grid_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class LichiGridPage extends StatefulWidget {
@@ -9,6 +10,30 @@ class LichiGridPage extends StatefulWidget {
 class _LichiGridPageState extends State<LichiGridPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.grey[100],
+        centerTitle: true,
+        title: Text(
+          "Lichi",
+          style: GoogleFonts.firaCode(
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          Icon(
+            Icons.add_shopping_cart,
+            color: Colors.black,
+          ),
+        ],
+        leading: IconButton(
+          color: Colors.black,
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+      ),
+
+    );
   }
 }
