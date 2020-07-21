615c6a378b024849933ae1bb995b92cbfe0b539b
Dreamwalker <aristojeff@gmail.com>
2020-07-21 20:23:48 +0900

    Update lichi_grid_page.dart

---
+++ lib/ep517_lichi_app_clone/lichi_grid_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class LichiGridPage extends StatefulWidget {
+  @override
+  _LichiGridPageState createState() => _LichiGridPageState();
+}
+
+class _LichiGridPageState extends State<LichiGridPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
