b7f6135b8e32bc48139a1ae5d4720b840d9594d0
Dreamwalker <aristojeff@gmail.com>
2020-07-22 20:08:28 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ep517_lichi_app_clone/lichi_app.dart';
+import 'ep520_smart_home_app/smart_home_app.dart';
 
 void main() {
-  runApp(LichiAppClone());
+  runApp(SmartHomeApp());
 }
