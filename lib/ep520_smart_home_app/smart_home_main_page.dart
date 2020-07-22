083ba5f78c38f4b532755b1eae7e7376a9acd2a4
Dreamwalker <aristojeff@gmail.com>
2020-07-22 20:09:32 +0900

    Update smart_home_main_page.dart

--- lib/ep520_smart_home_app/smart_home_main_page.dart
+++ lib/ep520_smart_home_app/smart_home_main_page.dart
@@ -8,6 +8,6 @@ class SmartHomeMainPage extends StatefulWidget {
 class _SmartHomeMainPageState extends State<SmartHomeMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
