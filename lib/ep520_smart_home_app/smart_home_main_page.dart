dcd902a26715bb1e4a42f6913fe5d79f53eb66f8
Dreamwalker <aristojeff@gmail.com>
2020-07-22 20:10:35 +0900

    Update smart_home_main_page.dart

--- lib/ep520_smart_home_app/smart_home_main_page.dart
+++ lib/ep520_smart_home_app/smart_home_main_page.dart
@@ -12,7 +12,12 @@ class _SmartHomeMainPageState extends State<SmartHomeMainPage> {
       backgroundColor: Colors.brown[50],
       body: Stack(
         children: [
-          
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 100,
+          )
         ],
       ),
     );
