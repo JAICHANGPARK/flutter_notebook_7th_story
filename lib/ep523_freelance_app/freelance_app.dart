ad5c5f7edffb65dd48ed5ab283fc57e29445eee0
Dreamwalker <aristojeff@gmail.com>
2020-07-25 19:57:33 +0900

    Update freelance_app.dart

--- lib/ep523_freelance_app/freelance_app.dart
+++ lib/ep523_freelance_app/freelance_app.dart
@@ -4,6 +4,20 @@ import 'package:flutter/material.dart';
 class FreelanceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: FreelanceMainScreen(),
+    );
+  }
+}
+
+class FreelanceMainScreen extends StatefulWidget {
+  @override
+  _FreelanceMainScreenState createState() => _FreelanceMainScreenState();
+}
+
+class _FreelanceMainScreenState extends State<FreelanceMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
