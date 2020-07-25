8f1b229daebc7f3555bb47abe0e897a0a3eb2476
Dreamwalker <aristojeff@gmail.com>
2020-07-25 19:56:46 +0900

    Update freelance_app.dart

--- lib/ep523_freelance_app/freelance_app.dart
+++ lib/ep523_freelance_app/freelance_app.dart
@@ -4,6 +4,6 @@ import 'package:flutter/material.dart';
 class FreelanceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp();
   }
 }
