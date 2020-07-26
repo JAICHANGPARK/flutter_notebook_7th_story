f774a71ee263f582367fe1053ab21ac2e18eef7f
Dreamwalker <aristojeff@gmail.com>
2020-07-26 19:54:27 +0900

    Update top_badges.dart

--- lib/ep523_freelance_app/top_badges.dart
+++ lib/ep523_freelance_app/top_badges.dart
@@ -5,8 +5,10 @@ class TopBadgesItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
+        Spacer(),
         Container(
           width: 38,
+          height: 48,
           decoration: BoxDecoration(
             color: Colors.red
           ),
