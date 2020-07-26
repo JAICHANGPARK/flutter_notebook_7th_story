cef627d4537360c5e2d234a456d8a3f125558dd7
Dreamwalker <aristojeff@gmail.com>
2020-07-26 19:57:20 +0900

    Update top_badges.dart

--- lib/ep523_freelance_app/top_badges.dart
+++ lib/ep523_freelance_app/top_badges.dart
@@ -15,7 +15,30 @@ class TopBadgesItem extends StatelessWidget {
           ),
           child: Stack(
             children: [
-              
+              Positioned(
+                left: 0,
+                top: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 0,
+                top: 0,
+                child: Container(
+                  height: 8,
+                  width: 8,
+                  decoration: BoxDecoration(
+                    color: Colors.red
+                  ),
+                ),
+              )
+
             ],
           ),
         ),
