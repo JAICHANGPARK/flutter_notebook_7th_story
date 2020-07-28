bf7a86973743afc5b149685995ef0d88cd343642
Dreamwalker <aristojeff@gmail.com>
2020-07-28 19:02:41 +0900

    Update freelance_app.dart

--- lib/ep523_freelance_app/freelance_app.dart
+++ lib/ep523_freelance_app/freelance_app.dart
@@ -80,14 +80,14 @@ class _FreelanceMainScreenState extends State<FreelanceMainScreen> {
                                   "Yesterday",
                                   style: TextStyle(fontSize: 12, color: Colors.grey),
                                 ),
-                                
+                                SizedBox(
+                                  height: 16,
+                                ),
                                 Row(
                                   children: [
                                     Text(
                                       "\$",
-                                      style: TextStyle(fontSize: 24,
-                                      color: Colors.grey),
-
+                                      style: TextStyle(fontSize: 24, color: Colors.grey),
                                     ),
                                     Text(
                                       "1,025",
