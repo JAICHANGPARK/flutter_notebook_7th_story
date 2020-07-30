124e989ca185594a69fffc89c3b2f389824886b0
Dreamwalker <aristojeff@gmail.com>
2020-07-30 20:58:13 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ep523_freelance_app/freelance_app.dart';
+import 'ep528_yoga_app/yoga_app.dart';
 
 void main() {
-  runApp(FreelanceApp());
+  runApp(YogaApp());
 }
