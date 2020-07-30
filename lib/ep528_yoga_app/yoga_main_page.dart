4a45970771c9ee03870031e3eabea434b42a7b3d
Dreamwalker <aristojeff@gmail.com>
2020-07-30 21:00:52 +0900

    Update yoga_main_page.dart

--- lib/ep528_yoga_app/yoga_main_page.dart
+++ lib/ep528_yoga_app/yoga_main_page.dart
@@ -13,6 +13,7 @@ class _YogaMainPageState extends State<YogaMainPage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
       ),
       drawer: Drawer(),
       body: SingleChildScrollView(
