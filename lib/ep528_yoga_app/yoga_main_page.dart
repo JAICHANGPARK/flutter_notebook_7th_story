202ac837de95a26ece0a01902a2fff2999e3e57c
Dreamwalker <aristojeff@gmail.com>
2020-07-30 20:56:50 +0900

    Create yoga_main_page.dart

---
+++ lib/ep528_yoga_app/yoga_main_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class YogaMainPage extends StatefulWidget {
+  @override
+  _YogaMainPageState createState() => _YogaMainPageState();
+}
+
+class _YogaMainPageState extends State<YogaMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
