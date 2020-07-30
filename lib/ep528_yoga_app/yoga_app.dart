779d60145114c7d68b3981a48a44847887198b70
Dreamwalker <aristojeff@gmail.com>
2020-07-30 20:55:58 +0900

    Update yoga_app.dart

---
+++ lib/ep528_yoga_app/yoga_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class YogaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: YogaMainPage(),
+    );
+  }
+}
