162e8835c7111d6c7f139f32586ee6defa46969b
Dreamwalker <aristojeff@gmail.com>
2020-08-03 20:12:35 +0900

    Update detail_page.dart

--- lib/ep528_yoga_app/detail_page.dart
+++ lib/ep528_yoga_app/detail_page.dart
@@ -16,7 +16,6 @@ class _DetailPageState extends State<DetailPage> {
     _videoPlayerController = VideoPlayerController.network(
         "https://player.vimeo.com/external/142621447.mobile.mp4?s=029702dbab1598537678ebfc7a6f500bc160608a&profile_id=116")
       ..initialize().then((value) async {
-
         setState(() {});
       });
   }
@@ -43,13 +42,18 @@ class _DetailPageState extends State<DetailPage> {
                 child: Stack(children: [
                   VideoPlayer(_videoPlayerController),
                   InkWell(
-                    onTap: (){
+                    onTap: () {
                       _videoPlayerController.play();
                     },
                     child: Center(
-                      child: CircleAvatar(radius: 48, backgroundColor: Colors.white.withOpacity(0.4),
-                      child: Icon(Icons.play_arrow, color: Colors.black,),),
-
+                      child: CircleAvatar(
+                        radius: 48,
+                        backgroundColor: Colors.white.withOpacity(0.4),
+                        child: Icon(
+                          Icons.play_arrow,
+                          color: Colors.black,
+                        ),
+                      ),
                     ),
                   ),
                 ]),
@@ -59,32 +63,36 @@ class _DetailPageState extends State<DetailPage> {
               padding: const EdgeInsets.all(8.0),
               child: Row(
                 children: [
-                  Text("Crane Pose", style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold
-                  ),),
+                  Text(
+                    "Crane Pose",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
                   Spacer(),
                   Icon(Icons.chat_bubble_outline),
                   Text("2,102"),
-                  SizedBox(width: 4,),
+                  SizedBox(
+                    width: 4,
+                  ),
                   Icon(Icons.favorite_border),
                   Text("2,102")
                 ],
               ),
             ),
-            SizedBox(height: 24,),
+            SizedBox(
+              height: 24,
+            ),
             Container(
               height: MediaQuery.of(context).size.height / 4,
-
               padding: EdgeInsets.all(9),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Expanded(
                     flex: 2,
-                    child: Text("Great For", style: TextStyle(
-                      fontSize: 18
-                    ),),
+                    child: Text(
+                      "Great For",
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
                   Expanded(
                     flex: 10,
@@ -107,24 +115,27 @@ class _DetailPageState extends State<DetailPage> {
             ),
             Container(
               height: MediaQuery.of(context).size.height / 4,
-
               padding: EdgeInsets.all(9),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Expanded(
                     flex: 2,
-                    child: Text("About this pose", style: TextStyle(
-                        fontSize: 18
-                    ),),
+                    child: Text(
+                      "About this pose",
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
                   Expanded(
                     flex: 10,
-                    child: Row(
-                      children: [
-
-                      ],
-                    ),
+                    child: Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do"
+                            " eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+                            "Ut enim ad minim veniam, quis nostrud exercitation ullamco labo"
+                            "ris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor "
+                            "in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla p"
+                            "ariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui offi"
+                            "cia deserunt mollit anim id est laborum."),
                   )
                 ],
               ),
@@ -135,16 +146,3 @@ class _DetailPageState extends State<DetailPage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
