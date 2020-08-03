3e6cb828be9afd68a832bf2f5ffa7f23a0c70bed
Dreamwalker <aristojeff@gmail.com>
2020-08-03 20:01:43 +0900

    Update detail_page.dart

--- lib/ep528_yoga_app/detail_page.dart
+++ lib/ep528_yoga_app/detail_page.dart
@@ -8,17 +8,18 @@ class DetailPage extends StatefulWidget {
 
 class _DetailPageState extends State<DetailPage> {
   VideoPlayerController _videoPlayerController;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _videoPlayerController = VideoPlayerController.network("dataSource")..initialize().then((value) {
-      setState(() {
-        
+    _videoPlayerController = VideoPlayerController.network(
+        "https://player.vimeo.com/external/142621447.mobile.mp4?s=029702dbab1598537678ebfc7a6f500bc160608a&profile_id=116")
+      ..initialize().then((value) {
+        setState(() {});
       });
-
-    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,10 +34,30 @@ class _DetailPageState extends State<DetailPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-
+            Container(
+              height: MediaQuery.of(context).size.height / 3,
+              decoration: BoxDecoration(
+                color: Colors.blue
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
