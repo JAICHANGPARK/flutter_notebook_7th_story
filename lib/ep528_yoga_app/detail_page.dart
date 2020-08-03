b6e68c6aa8e3b3f7bc51ae8c60f95c9ad70b2543
Dreamwalker <aristojeff@gmail.com>
2020-08-03 19:53:48 +0900

    Update detail_page.dart

--- lib/ep528_yoga_app/detail_page.dart
+++ lib/ep528_yoga_app/detail_page.dart
@@ -9,6 +9,14 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.grey),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
       body: SingleChildScrollView(),
     );
   }
