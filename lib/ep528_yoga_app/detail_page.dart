1f98f8dc298be8ebd31aa33eec6f2edfdc206092
Dreamwalker <aristojeff@gmail.com>
2020-08-03 19:59:42 +0900

    Update detail_page.dart

--- lib/ep528_yoga_app/detail_page.dart
+++ lib/ep528_yoga_app/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class DetailPage extends StatefulWidget {
   @override
@@ -6,6 +7,18 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  VideoPlayerController _videoPlayerController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _videoPlayerController = VideoPlayerController.network("dataSource")..initialize().then((value) {
+      setState(() {
+        
+      });
+
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
