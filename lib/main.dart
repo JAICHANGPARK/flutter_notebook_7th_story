aded6132524cbb6620690797fcfad245596c4394
Dreamwalker <aristojeff@gmail.com>
2020-08-04 20:33:59 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'ep523_freelance_app/freelance_app.dart';
-import 'ep528_yoga_app/yoga_app.dart';
+import 'package:flutter_notebook_7th_story/ep533_finance_app/finance_app.dart';
+
 
 void main() {
-  runApp(YogaApp());
+  runApp(FinanceApp());
 }
