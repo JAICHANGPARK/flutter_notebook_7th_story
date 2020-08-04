b0426b1659b334b67b8cf75cde0816451fd01852
Dreamwalker <aristojeff@gmail.com>
2020-08-04 20:34:46 +0900

    Create finance_main_pge.dart

---
+++ lib/ep533_finance_app/finance_main_pge.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class FinanceMainPage extends StatefulWidget {
+  @override
+  _FinanceMainPageState createState() => _FinanceMainPageState();
+}
+
+class _FinanceMainPageState extends State<FinanceMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
