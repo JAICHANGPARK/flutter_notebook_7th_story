9fe48cab6caed006506d302f6a3897c0afd2bd95
Dreamwalker <aristojeff@gmail.com>
2020-08-06 21:18:47 +0900

    Update finance_main_pge.dart

--- lib/ep533_finance_app/finance_main_pge.dart
+++ lib/ep533_finance_app/finance_main_pge.dart
@@ -141,8 +141,13 @@ class _FinanceMainPageState extends State<FinanceMainPage> {
                     child: Container(
                       margin: EdgeInsets.symmetric(horizontal: 16),
                       decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(12)),
+                      padding: EdgeInsets.all(8),
                       child: Column(
                         children: [
+                          Row(
+
+                          ),
+                          Row()
 
                         ],
                       ),
