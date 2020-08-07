6d4eadba2aaf8643b6dda255450c951000888c6e
Dreamwalker <aristojeff@gmail.com>
2020-08-07 20:35:40 +0900

    Update home_pick_main_page.dart

---
+++ lib/ep536_home_pick_app/home_pick_main_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePickMainPage extends StatefulWidget {
+  @override
+  _HomePickMainPageState createState() => _HomePickMainPageState();
+}
+
+class _HomePickMainPageState extends State<HomePickMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
