2cfff940ee2030dc43c296310d909780adc5b917
Dreamwalker <aristojeff@gmail.com>
2020-08-11 21:11:02 +0900

    Update user_matching_app.dart

--- lib/ep540_user_matching_app/user_matching_app.dart
+++ lib/ep540_user_matching_app/user_matching_app.dart
@@ -3,6 +3,21 @@ import 'package:flutter/material.dart';
 class UserMatchingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: UserMatchingHomePage(),
+    );
   }
 }
+
+class UserMatchingHomePage extends StatefulWidget {
+  @override
+  _UserMatchingHomePageState createState() => _UserMatchingHomePageState();
+}
+
+class _UserMatchingHomePageState extends State<UserMatchingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
