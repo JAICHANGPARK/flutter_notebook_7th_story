55d4904b06b449d64482294640d00e5ff016da8b
Dreamwalker <aristojeff@gmail.com>
2020-08-12 20:33:17 +0900

    Update user_matching_app.dart

--- lib/ep540_user_matching_app/user_matching_app.dart
+++ lib/ep540_user_matching_app/user_matching_app.dart
@@ -16,11 +16,12 @@ class UserMatchingHomePage extends StatefulWidget {
 
 class _UserMatchingHomePageState extends State<UserMatchingHomePage> {
   final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _globalKey,
-      drawer: Drawer(),
+      drawer: CustomDrawer(),
       body: SafeArea(
         child: Stack(
           children: [
@@ -40,38 +41,41 @@ class _UserMatchingHomePageState extends State<UserMatchingHomePage> {
                       Expanded(
                           child: Row(
                         children: [
-                          Expanded(child: Center(
+                          Expanded(
+                              child: Center(
                             child: IconButton(
                               icon: Icon(Icons.chat_bubble_outline),
                               iconSize: 38,
                               color: Colors.blue[200],
-                              onPressed: () {  },
+                              onPressed: () {},
                             ),
                           )),
-                          Expanded(child: Center(
+                          Expanded(
+                              child: Center(
                             child: IconButton(
                               icon: Icon(Icons.search),
                               iconSize: 38,
                               color: Colors.blue[200],
-                              onPressed: () {  },
+                              onPressed: () {},
                             ),
                           )),
-                          Expanded(child: Center(
+                          Expanded(
+                              child: Center(
                             child: IconButton(
                               icon: Icon(Icons.favorite_border),
                               iconSize: 38,
                               color: Colors.blue[200],
-                              onPressed: () {  },
+                              onPressed: () {},
                             ),
                           )),
-                          Expanded(child: Center(
+                          Expanded(
+                              child: Center(
                             child: IconButton(
                               icon: Icon(Icons.menu),
                               iconSize: 38,
                               color: Colors.blue[200],
                               onPressed: () {
                                 _globalKey.currentState.openDrawer();
-
                               },
                             ),
                           )),
@@ -98,5 +102,73 @@ class _UserMatchingHomePageState extends State<UserMatchingHomePage> {
         ),
       ),
     );
+  }
+}
+
+class CustomDrawer extends StatelessWidget {
+  /// Creates a material design drawer.
+  ///
+  /// Typically used in the [Scaffold.drawer] property.
+  ///
+  /// The [elevation] must be non-negative.
+  const CustomDrawer({Key key, this.elevation = 16.0, this.child, this.semanticLabel, this.width})
+      : assert(elevation != null && elevation >= 0.0),
+        super(key: key);
+
+  /// The z-coordinate at which to place this drawer relative to its parent.
+  ///
+  /// This controls the size of the shadow below the drawer.
+  ///
+  /// Defaults to 16, the appropriate elevation for drawers. The value is
+  /// always non-negative.
+  final double elevation;
+
+  final double width;
+
+  /// The widget below this widget in the tree.
+  ///
+  /// Typically a [SliverList].
+  ///
+  /// {@macro flutter.widgets.child}
+  final Widget child;
+
+  /// The semantic label of the dialog used by accessibility frameworks to
+  /// announce screen transitions when the drawer is opened and closed.
+  ///
+  /// If this label is not provided, it will default to
+  /// [MaterialLocalizations.drawerLabel].
+  ///
+  /// See also:
+  ///
+  ///  * [SemanticsConfiguration.namesRoute], for a description of how this
+  ///    value is used.
+  final String semanticLabel;
+
+  @override
+  Widget build(BuildContext context) {
+    assert(debugCheckHasMaterialLocalizations(context));
+    String label = semanticLabel;
+    switch (Theme.of(context).platform) {
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+        label = semanticLabel;
+        break;
+      case TargetPlatform.android:
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        label = semanticLabel ?? MaterialLocalizations.of(context)?.drawerLabel;
+    }
+    return Semantics(
+      scopesRoute: true,
+      namesRoute: true,
+      explicitChildNodes: true,
+      label: label,
+      child: ConstrainedBox(constraints: const BoxConstraints.expand(width: 340),
+      child: Material(
+        elevation: elevation,
+        child: child,
+      ),
+    ));
   }
 }
