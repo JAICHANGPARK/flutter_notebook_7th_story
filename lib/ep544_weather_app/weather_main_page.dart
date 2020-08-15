8d5807ec0dc465a79468246369704afeab6638c2
Dreamwalker <aristojeff@gmail.com>
2020-08-15 19:01:12 +0900

    Update weather_main_page.dart

--- lib/ep544_weather_app/weather_main_page.dart
+++ lib/ep544_weather_app/weather_main_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class WeatherMainPage extends StatefulWidget {
+  @override
+  _WeatherMainPageState createState() => _WeatherMainPageState();
+}
+
+class _WeatherMainPageState extends State<WeatherMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
