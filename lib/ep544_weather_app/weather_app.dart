f8967880ea8501914b3768eb0c8b08977c3bfa08
Dreamwalker <aristojeff@gmail.com>
2020-08-15 19:00:45 +0900

    Update weather_app.dart

--- lib/ep544_weather_app/weather_app.dart
+++ lib/ep544_weather_app/weather_app.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: WeatherMainPage(),
+    );
   }
 }
