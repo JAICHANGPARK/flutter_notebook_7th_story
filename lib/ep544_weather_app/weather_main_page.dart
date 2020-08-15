dc828ab2586b182678ee0623356672ffa7e11ad2
Dreamwalker <aristojeff@gmail.com>
2020-08-15 19:04:11 +0900

    Update weather_main_page.dart

--- lib/ep544_weather_app/weather_main_page.dart
+++ lib/ep544_weather_app/weather_main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherMainPage extends StatefulWidget {
@@ -11,12 +13,23 @@ class _WeatherMainPageState extends State<WeatherMainPage> {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage("https://cdn.pixabay.com/photo/2018/05/30/00/24/thunderstorm-3440450_960_720.jpg"),
-                fit: BoxFit.cover
-              )
+          Positioned(
+            left: 0,
+            top: 0,
+            bottom: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image:
+                          NetworkImage("https://cdn.pixabay.com/photo/2018/05/30/00/24/thunderstorm-3440450_960_720.jpg"),
+                      fit: BoxFit.cover)),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 5.0,
+                  sigmaY: 5.0,
+                ),
+              ),
             ),
           )
         ],
