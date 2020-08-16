a3a55fbda15df36983ada6276c1065a4ce7fbb43
Dreamwalker <aristojeff@gmail.com>
2020-08-16 18:05:02 +0900

    Update weather_main_page.dart

--- lib/ep544_weather_app/weather_main_page.dart
+++ lib/ep544_weather_app/weather_main_page.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 const Color _textColor = Colors.white;
+
 class WeatherMainPage extends StatefulWidget {
   @override
   _WeatherMainPageState createState() => _WeatherMainPageState();
 }
 
 class _WeatherMainPageState extends State<WeatherMainPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,14 +24,13 @@ class _WeatherMainPageState extends State<WeatherMainPage> {
             child: Container(
               decoration: BoxDecoration(
                   image: DecorationImage(
-                      image:
-                          NetworkImage("https://cdn.pixabay.com/photo/2019/05/28/03/23/thunderstorm-4234254_960_720.jpg"),
+                      image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2019/05/28/03/23/thunderstorm-4234254_960_720.jpg"),
                       fit: BoxFit.cover)),
               child: BackdropFilter(
                 filter: ImageFilter.blur(
                   sigmaX: 3.0,
                   sigmaY: 3.0,
-
                 ),
                 child: Container(
                   color: Colors.black.withOpacity(0.2),
@@ -51,55 +50,33 @@ class _WeatherMainPageState extends State<WeatherMainPage> {
                       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
                       child: Row(
                         children: [
-                          Icon(Icons.access_time, color: _textColor,size: 12,),
-                          SizedBox(width: 8,),
-                          Text("07:32 AM",  style: GoogleFonts.montserrat(
-                              color: _textColor
-                          )),
+                          Icon(
+                            Icons.access_time,
+                            color: _textColor,
+                            size: 12,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text("07:32 AM", style: GoogleFonts.montserrat(color: _textColor)),
                           Spacer(),
-                          Icon(Icons.edit_location, color: _textColor,size: 12,),
-                          SizedBox(width: 8,),
-                          Text("New york", style: GoogleFonts.montserrat(
-                            color: _textColor
-                          ))
+                          Icon(
+                            Icons.edit_location,
+                            color: _textColor,
+                            size: 12,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text("New york", style: GoogleFonts.montserrat(color: _textColor))
                         ],
                       ),
                     )
-
                   ],
                 ),
-              ) )
+              ))
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
