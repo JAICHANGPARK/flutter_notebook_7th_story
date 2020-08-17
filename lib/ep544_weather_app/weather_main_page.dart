6ef2344b7d79b16db5e5742befa9cd407d402384
Dreamwalker <aristojeff@gmail.com>
2020-08-17 20:17:56 +0900

    Update weather_main_page.dart

--- lib/ep544_weather_app/weather_main_page.dart
+++ lib/ep544_weather_app/weather_main_page.dart
@@ -185,7 +185,7 @@ class _WeatherMainPageState extends State<WeatherMainPage> {
                       ),
                     ),
                     Container(),
-                    
+
                   ],
                 ),
               )),
