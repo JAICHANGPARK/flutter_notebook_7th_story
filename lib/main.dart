afede6bba1e54823bd52343124f2459c45f3512c
Dreamwalker <aristojeff@gmail.com>
2020-08-18 18:32:48 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'ep547_home_app/home_app.dart';
 
 void main() {
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.white
+  ));
+
   runApp(HomeApp());
 }
