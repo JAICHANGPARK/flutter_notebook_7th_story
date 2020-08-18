b1cb0176d91e5fb76ab89687bbf282f31725ae41
Dreamwalker <aristojeff@gmail.com>
2020-08-18 18:38:58 +0900

    Update home_app.dart

--- lib/ep547_home_app/home_app.dart
+++ lib/ep547_home_app/home_app.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 class HomeApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,64 +20,38 @@ class _HomeListPageState extends State<HomeListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    resizeToAvoidBottomPadding: false,
+      resizeToAvoidBottomPadding: false,
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.white,
         centerTitle: true,
-        title: Text("Feed", style: TextStyle(
-          color: Colors.black
-        ),),
+        title: Text(
+          "Feed",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: SafeArea(child: Stack(
-        children: [
-          Positioned(child:
-          Placeholder()),
-          Positioned(
-              left: 16,
-              right: 16,
-              bottom: 24,
-              child: Container(
-                height: 58,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(32),
-                  boxShadow: [
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(child: Placeholder()),
+            Positioned(
+                left: 16,
+                right: 16,
+                bottom: 24,
+                child: Container(
+                  height: 58,
+                  decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(32), boxShadow: [
                     BoxShadow(
                       offset: Offset(0, 4),
                       color: Colors.black.withOpacity(.2),
                       blurRadius: 3,
-                      spreadRadius:2,
+                      spreadRadius: 2,
                     )
-                  ]
-                ),
-              ))
-        ],
-      ),),
+                  ]),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
