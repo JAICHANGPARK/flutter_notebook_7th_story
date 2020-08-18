d9149aa8ec457f36a50285c10916c6bbd23fdfd9
Dreamwalker <aristojeff@gmail.com>
2020-08-18 18:29:27 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_7th_story/ep544_weather_app/weather_app.dart';
+import 'ep547_home_app/home_app.dart';
 
 void main() {
-  runApp(WeatherApp());
+  runApp(HomeApp());
 }
