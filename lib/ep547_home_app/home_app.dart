75d7acde37d28ad4d4121e78a17d24782552e625
Dreamwalker <aristojeff@gmail.com>
2020-08-18 18:33:37 +0900

    Update home_app.dart

--- lib/ep547_home_app/home_app.dart
+++ lib/ep547_home_app/home_app.dart
@@ -8,6 +8,7 @@ class HomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeListPage(),
     );
   }
