e164317fecb85a2696ef2576f44bcebe7e066132
Dreamwalker <aristojeff@gmail.com>
2020-08-18 18:28:57 +0900

    Update home_app.dart

--- lib/ep547_home_app/home_app.dart
+++ lib/ep547_home_app/home_app.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class HomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: HomeListPage(),
+    );
   }
 }
