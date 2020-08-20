c93aec3704c3e815a74508a7cf6372311cf37723
Dreamwalker <aristojeff@gmail.com>
2020-08-20 21:55:33 +0900

    Create home_item.dart

---
+++ lib/ep547_home_app/home_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class HomeItem extends StatefulWidget {
+  @override
+  _HomeItemState createState() => _HomeItemState();
+}
+
+class _HomeItemState extends State<HomeItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
