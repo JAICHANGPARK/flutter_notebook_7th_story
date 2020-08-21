98fbc511f99ccb83adc16e9b30eca0cffd7c0fa6
Dreamwalker <aristojeff@gmail.com>
2020-08-21 23:24:35 +0900

    Update home_item.dart

--- lib/ep547_home_app/home_item.dart
+++ lib/ep547_home_app/home_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_7th_story/ep547_home_app/home_detail_page.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomeItem extends StatefulWidget {
@@ -19,101 +20,108 @@ class _HomeItemState extends State<HomeItem> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 16),
-      child: Container(
-        height: 340,
-        width: double.infinity,
-        child: Card(
-          child: Column(
-            children: [
-              Expanded(
-                  flex: 4,
-                  child: Container(
-                    child: Stack(
-                      children: [
-                        Positioned(
-                            left: 0,
-                            top: 0,
-                            right: 0,
-                            bottom: 0,
-                            child: PageView(
-                              controller: _pageController,
-                              children: [
-                                ...imgItems.map((e) =>
-                                    Image.network(
-                                      e,
-                                      fit: BoxFit.cover,
-                                    ),
-                                ).toList()
-
-                             
-                              ],
-                            )),
-                        Positioned(
-                            right: 12,
-                            top: 12,
-                            child: CircleAvatar(
-                              backgroundColor: Colors.white,
-                              child: Icon(
-                                Icons.favorite_border,
-                                color: Colors.black,
-                              ),
-                            )),
-                        Positioned(
-                            bottom: 8,
-                            left: 0,
-                            right: 0,
-                            child: Center(
-                              child: SmoothPageIndicator(
+      child: GestureDetector(
+        onTap: (){
+          Navigator.of(context).push(MaterialPageRoute(builder: (context)=> HomeDetailPage(
+            imgItems,
+          )));
+        },
+        child: Container(
+          height: 340,
+          width: double.infinity,
+          child: Card(
+            child: Column(
+              children: [
+                Expanded(
+                    flex: 4,
+                    child: Container(
+                      child: Stack(
+                        children: [
+                          Positioned(
+                              left: 0,
+                              top: 0,
+                              right: 0,
+                              bottom: 0,
+                              child: PageView(
                                 controller: _pageController,
-                                count: 5,
-                                effect: WormEffect(
-                                    dotHeight: 8,
-                                    dotWidth: 8,
-                                    activeDotColor: Colors.white,
-                                    dotColor: Colors.white.withOpacity(0.5)),
-                              ),
-                            ))
-                      ],
-                    ),
-                  )),
-              Expanded(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Chip(
-                          label: Text(
-                            "NEW",
-                            style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold),
-                          ),
-                          backgroundColor: Colors.purple[50],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "3,400\$",
-                              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                                children: [
+                                  ...imgItems.map((e) =>
+                                      Image.network(
+                                        e,
+                                        fit: BoxFit.cover,
+                                      ),
+                                  ).toList()
+
+
+                                ],
+                              )),
+                          Positioned(
+                              right: 12,
+                              top: 12,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.white,
+                                child: Icon(
+                                  Icons.favorite_border,
+                                  color: Colors.black,
+                                ),
+                              )),
+                          Positioned(
+                              bottom: 8,
+                              left: 0,
+                              right: 0,
+                              child: Center(
+                                child: SmoothPageIndicator(
+                                  controller: _pageController,
+                                  count: 5,
+                                  effect: WormEffect(
+                                      dotHeight: 8,
+                                      dotWidth: 8,
+                                      activeDotColor: Colors.white,
+                                      dotColor: Colors.white.withOpacity(0.5)),
+                                ),
+                              ))
+                        ],
+                      ),
+                    )),
+                Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Chip(
+                            label: Text(
+                              "NEW",
+                              style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold),
                             ),
-                            Text("154m^2", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                            Text("4.5 Room", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                        Text(
-                          "Steinsche Gasse 32b, 47059 Duisburg, Sta",
-                          overflow: TextOverflow.clip,
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
+                            backgroundColor: Colors.purple[50],
                           ),
-                        ),
-                      ],
-                    ),
-                  ))
-            ],
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "3,400\$",
+                                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                              ),
+                              Text("154m^2", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                              Text("4.5 Room", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                          Text(
+                            "Steinsche Gasse 32b, 47059 Duisburg, Sta",
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
